f9ad4756ed19dbef277b3f28e74b3a9a0224c56a
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-08-13 10:31:11 +0530

    feat: basic css component

---
+++ src/components/Button.jsx
@@ -0,0 +1,4 @@
+export default function Button() {
+    return <button class="hover-button">Hover Me</button>;
+  }
+  
--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -3,7 +3,16 @@ export default function ItemCard(){
 
     return(
         <div>
-            
+      <div class="card">
+        <img
+          src="https://lh3.googleusercontent.com/hwau7OVWx96XaME5KpRuJ0I_MscrerK6SbRH1UwYHYaxIDQQtn7RZK02LDSfBzCreidFgDsJeXyqDct6EZiH6vsV=w640-h400-e365-rj-sc0x00ffffff"
+          alt="Card Image"
+        />
+        <div class="card-details">
+          <h2>Title</h2>
+          <p>Description</p>
         </div>
+      </div>
+    </div>
     )
 }
