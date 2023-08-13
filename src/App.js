6980f486b5f9a001cffd4da3847b19ff43f81c35
Sourav-nath <35804676+Programmer-Sourav@users.noreply.github.com>
2023-08-13 10:22:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
+import ViewDetails from './pages/ViewDetails';
 
 function App() {
   return (
     <div className="App">
      <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/details/:id' element={<ViewDetails/>}/>
      </Routes>
     </div>
   );
